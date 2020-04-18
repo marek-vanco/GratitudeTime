d2369682c549da90869a3341b03b6435eef5f178
Marek Vančo <marek.vanco@stellisee.com>
2020-04-18 17:31:42 +0200

    Refactoring of differents classes and binding forced value (compile and run now)

--- app/src/main/java/com/stellisee/gratitudetime/ui/citation/CitationActivity.kt
+++ app/src/main/java/com/stellisee/gratitudetime/ui/citation/CitationActivity.kt
@@ -2,15 +2,16 @@ package com.stellisee.gratitudetime.ui.citation
 
 import android.content.res.Configuration
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.stellisee.gratitudetime.R
+import com.stellisee.gratitudetime.databinding.ActivityCitationBinding
 
 class CitationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_citation)
+        val activityCitationBinding = ActivityCitationBinding.inflate(layoutInflater)
+        val view = activityCitationBinding.root
+        setContentView(view)
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
--- app/src/main/java/com/stellisee/gratitudetime/ui/citation/CitationFragment.kt
+++ app/src/main/java/com/stellisee/gratitudetime/ui/citation/CitationFragment.kt
@@ -5,30 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.stellisee.gratitudetime.databinding.FragmentCitationBinding
 import com.stellisee.gratitudetime.viewmodel.CitationViewModel
-import kotlinx.android.synthetic.main.fragment_citation.view.*
+import com.stellisee.gratitudetime.viewmodel.CitationViewModelFactory
 import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
 import org.kodein.di.generic.instance
 
 class CitationFragment() : Fragment(), KodeinAware {
 
-    override val kodein by kodein()
-
-    private val citationViewModel by instance<CitationViewModel>()
-    // private val citationViewModelFactory : CitationViewModelFactory by instance<CitationViewModelFactory>()
+    override val kodein by closestKodein()
+    private val citationViewModelFactory by instance<CitationViewModelFactory>()
+    private lateinit var citationViewModel : CitationViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        val binding = CitationFragmentBinding.inflate(inflater, container, false)
-        binding.viewModel = citationViewModel
-        binding.lifecycleOwner = this
-        return binding.root
+        val fragmentCitationBindging = FragmentCitationBinding.inflate(inflater, container, false)
+        fragmentCitationBindging.textViewAuthor.setText("author1")
+        return fragmentCitationBindging.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-
-
+        citationViewModel = citationViewModelFactory.create(CitationViewModel::class.java)
     }
 }
